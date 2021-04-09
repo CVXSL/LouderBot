2830b7345388e7add81facbd88ab68049abd23c5
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-04-09 16:04:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ bot.on('ready', () => {
 }
 )
 
+bot.on("message", async message => {
+    if (command === "say") {
 
+        if (!args[0]) return message.channel.send("What should i say?, try `*say <message>`")
+	if (message.content.includes("^")) {
+            return message.reply("ERROR: the "^" key is invalid!")
+        
+        }
+        const sayMessage = args.slice(0).join(" ");
+        message.delete().catch(O_o => { });
+        message.channel.send(sayMessage).catch(err => message.reply(`Something went wrong... ${err}`));
+        return;
+    };
+});
 
 // THIS IS THE bot.login
 
