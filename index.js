572713c6500f0b312f69733bc085c1ea0e5a0bd5
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-04-09 16:42:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const prefix = "%"
+const prefix = ""
 
 // THIS IS THE STATUS
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("${Client.guilds.cache.size} servers!", { 
+    bot.user.setActivity("for the \"say\" command!", { 
         type: "WATCHING", 
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
 
 }
 )
+
+//help command
+bot.on('message', async message => {
+    if (message.content === "%help") {
+	if (message.channel.type == "dm") return;
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('352256')
+            .setTitle('Help')
+            .setDescription('If you need help, join our support server and DM ``OS Utilities``. \n \n (" [``[Click here]``](https://discord.gg/wstKjrX)") \n \n *If you are looking for a list of commands, then use the ``=commands`` command!*')
+
+        message.channel.send(exampleEmbed);
+    }
+})
+
+//commands command
+bot.on('message', async message => {
+    if (message.content === "%commands") {
+	if (message.channel.type == "dm") return;
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('352256')
+            .setTitle('Commands')
+            .setDescription('**Say** = Just say the word ``say`` then say what you want the bot to say. (EX: ``say <message>`` \n **Help** = Use the ``%help`` command if you need help from out support server.')
+
+        message.channel.send(exampleEmbed);
+    }
+})
 
 bot.on("message", async message => {
 	
@@ -24,8 +50,9 @@ bot.on("message", async message => {
     const command = args.shift().toLowerCase();
 	
     if (command === "say") {
+	if (message.channel.type == "dm") return;
 
-        if (!args[0]) return message.channel.send("What should i say?, try `*say <message>`")
+        if (!args[0]) return message.channel.send("What should i say?, try `say <message>`")
 	if (message.content.includes("^")) {
             return message.reply("ERROR: the "^" key is invalid!")
         
