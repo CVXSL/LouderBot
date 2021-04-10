aeb64c31f1d0a5ca86b9efdbc34aee4b1b031a2e
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-04-10 07:03:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const prefix = ""
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("${bot.guild.size} servers!", { 
+    bot.user.setActivity("${<bot>.guild.size} servers!", { 
         type: "WATCHING", 
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
