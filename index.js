3fb2a8f1e7cd073e9819a3dabf801c7f7c83e80a
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-05-22 19:44:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const math = require('discord-math');
- 
-module.exports = {
-    name: "calculate",
-    description: "The bot does math for you"
-    run: async (bot, message, args) => {
-        try {
-            let num1 = args[0];
-            let operation = args [1];
-            let num2 = args[2];
-            
-            if (!num1) return message.channel.send('Num1 needs to be specified!');
-            if (!operation) return message.channel.send('An operation was not specified!');
-            if (!num2) return message.channel.send('Num2 needs to be specified!');
- 
-            message.channel.send(`Answer: ${math.calculate(num1, operation, num2)}`);
-        } catch (e) {
-            console.log(e);
-        }
-    }
-}
-
 const prefix = "%"
 
 // THIS IS THE STATUS
